d4141a2c5d87d57c0a0d1ae706644d3886b9e3d5
Peter Eisenmann <p3732@getgoogleoff.me>
2026-03-23 12:02:24 +0100

    window-slot: Drop unused bookmark getter

--- src/nautilus-window-slot.c
+++ src/nautilus-window-slot.c
@@ -3042,12 +3042,6 @@ nautilus_window_slot_get_current_view (NautilusWindowSlot *self)
     return self->content_view;
 }
 
-NautilusBookmark *
-nautilus_window_slot_get_bookmark (NautilusWindowSlot *self)
-{
-    return self->current_location_bookmark;
-}
-
 GList *
 nautilus_window_slot_get_back_history (NautilusWindowSlot *self)
 {
